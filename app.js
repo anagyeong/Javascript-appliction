2a511c5bf37cddab98211f2c255ab7cace047822
anagyeong <dhsdb603519@gmail.com>
2021-08-29 23:08:27 +0900

    change buttonEvnet to submitEvent

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const loginButton = document.querySelector(".login-form button");
+
+function handleLoginFormSubmit() {
+    const username = loginInput.value;
+    console.log(username)
+}
+
+loginForm.addEventListener("submit", handleLoginFormSubmit)
