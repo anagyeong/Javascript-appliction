200498fe76165cc8aeb2ac23ffa8cec0b0c08544
anagyeong <dhsdb603519@gmail.com>
2021-08-31 00:23:33 +0900

    add hello username page

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
+const greeting = document.querySelector(".greeting");
 
-function handleLoginFormSubmit() {
+const HIDDEN_CLASSNAME = "hidden";
+
+function handleLoginFormSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username)
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
 }
 
 loginForm.addEventListener("submit", handleLoginFormSubmit)
